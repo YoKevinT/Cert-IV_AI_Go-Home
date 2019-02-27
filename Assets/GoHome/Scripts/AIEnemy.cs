f755007b74bcadb2c296ff8228c4e0e9da0e48cc
Kevin Turcumani <kevinjaja98@gmail.com>
2019-02-27 12:06:52 +1100

    Artificial-Intelligence Added

---
+++ Assets/GoHome/Scripts/AIEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIEnemy : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent agent; // Reference to NavMeshAgent component
+
+    void Start()
+    {
+        // Getting NavMeshAgent from list of components
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Tell NavMeshAgent to follow target position
+        agent.SetDestination(target.position);
+    }
+}
